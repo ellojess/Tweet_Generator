def2f1471d6bc30ca8d09d80f9288976456bbf10
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-23 14:56:58 -0800

    add comments to code

--- rearrange.py
+++ rearrange.py
@@ -1,7 +1,7 @@
 import sys as sys 
 
-#get the command line arguments 
+#get the command line arguments with sys 
 if __name__ == "__main__":
     args = sys.argv
-    words = args[1:]
+    words = args[1:] #start with 1 to ignore first index 0
     print(words)
