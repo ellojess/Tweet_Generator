29349ff3300b8ac8754eebf2c91cb39ebf6fd08c
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-24 22:44:13 -0800

    edit rearrange function

--- rearrange.py
+++ rearrange.py
@@ -1,25 +1,27 @@
 import random
 import sys as sys 
+from python_quote import random_python_quote
 
 '''
 TODO: 
 - fix split words.txt list 
 - test with " python3 rearrange.py how now brown cow "
+- 
 '''
 
-sentence = sys.argv[1:]
+sentence = random_python_quote()
 
 def rearrange(sentence):
     words = sentence.split()
-    for word in words:
-        random.shuffle(words)
-        print(words)
-    return ' '.join(words)
-    print(words)
+    random.shuffle(words)
+    new_sentence = ' '.join(words)
+    print(new_sentence)
+
+# rearrange(sentence)
 
 # get the command line arguments with sys 
 if __name__ == "__main__":
     print(rearrange(sentence))
-    args = sys.argv
-    words = args[1:]
-    print(words)
+    # args = sys.argv
+    # words = args[1:]
+    # print(words)
