b59fa097f2795d1e9e687248ac674131e4db68f7
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-26 13:42:22 -0800

    add entry point to random_sentence() and add comments to code

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,1 +1,27 @@
-f=open("words.txt", "r")
+import random
+import sys as sys 
+
+'''
+'''
+
+words = open('words.txt', 'r')
+words = words.read().split('\n')
+sentence_len = int(sys.argv[1])
+print(sentence_len)
+
+'''
+Select a random set of words from words file and store in a data type
+Put the number of words requested together into a "sentence"
+Output sentence
+'''
+def random_sentence(sentence_len):
+    sentence = []
+    for word in range(sentence_len):
+        word = random.choice(words)
+        sentence.append(word)
+    return ' '.join(sentence)
+
+# f.close()
+
+if __name__ == "__main__":
+    print(random_sentence(sentence_len))
