47d7f1692489d6388323b58774d0e7c5670bb2f0
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-28 19:08:55 -0800

    implement unique_words function in word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,5 @@
 import random 
 
-
-
 '''
 function which takes a source_text argument
 (can be either a filename or the contents of 
@@ -12,8 +10,6 @@ number of times the word appears in the source text
 '''
 
 def histogram(words):
-
-    word_histogram = {}
 
     # Iterate over each word in line
     for word in words: 
@@ -33,9 +29,9 @@ def histogram(words):
 function that takes a histogram argument 
 and returns the total count of unique words in the histogram
 '''
-def unique_words():
+def unique_words(words):
     # count word_histogram keys 
-    pass 
+    print(len(word_histogram))
 
 '''
 function that takes a word and histogram 
@@ -50,8 +46,10 @@ if __name__ == "__main__":
     # lines = filehandle.readlines()
     # histogram(filehandle)
     # filehandle.close()
+    word_histogram = {}
 
     with open(filename, 'r') as f:
         words = f.read().split(' ')
         histogram(words)
+        unique_words(words)
     # print(words)    
