58740e92fb6bddcde683ddd49b4a4c0312e62d28
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-29 14:41:58 -0800

    add commentary to code

--- python_quote.py
+++ python_quote.py
@@ -1,14 +1,22 @@
 import random
 
+'''
+List of example quotes 
+'''
 quotes = ("It's just a flesh wound.",
           "He's not the Messiah. He's a very naughty boy!",
           "THIS IS AN EX-PARROT!!")
 
-
+'''
+Function returns a random quote from list 
+'''
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
 
+'''
+Entry point of program, runs random_python_question() and returns quote
+'''
 if __name__ == '__main__':
     quote = random_python_quote()
     print(quote)
--- quote_user.py
+++ quote_user.py
@@ -1,3 +1,6 @@
 from python_quote import random_python_quote
 
+'''
+Accesses random_python_quote and prints a single random quote from list
+'''
 print(random_python_quote())
