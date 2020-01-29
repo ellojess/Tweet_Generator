6318d540b772bfde2c4becbea913c8e8149c87df
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-28 21:43:56 -0800

    implement frequency() function in word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -8,9 +8,7 @@ and return a histogram data structure that
 stores each unique word along with the 
 number of times the word appears in the source text
 '''
-
 def histogram(words):
-
     # Iterate over each word in line
     for word in words: 
         # Check if the word is already in dictionary 
@@ -29,7 +27,7 @@ def histogram(words):
 function that takes a histogram argument 
 and returns the total count of unique words in the histogram
 '''
-def unique_words(words):
+def unique_words():
     # count word_histogram keys 
     print(len(word_histogram))
 
@@ -38,18 +36,25 @@ function that takes a word and histogram
 argument and returns the number of times that word appears in a text
 '''
 def frequency():
-    pass
+    # takes user input for a word they'd like to search 
+    search_word = input("Type word: ")
+    # search for word in dictionary, print out frequency of occurances 
+    if search_word in word_histogram:
+        print(f'{search_word} appears {word_histogram[search_word]}x')
+    else:
+        print("Word does not exist in text")
 
+'''
+Entry point of program, opens and closes words.txt file for program to use
+Holds empty dictionary for program to use
+'''
 if __name__ == "__main__":
     filename = 'words.txt'
-    # filehandle = open(filename, 'r') 
-    # lines = filehandle.readlines()
-    # histogram(filehandle)
-    # filehandle.close()
+
     word_histogram = {}
 
     with open(filename, 'r') as f:
         words = f.read().split(' ')
         histogram(words)
-        unique_words(words)
-    # print(words)    
+        unique_words()
+        frequency()
