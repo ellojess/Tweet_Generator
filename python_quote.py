54e2eb8ab33e2f74e10654382cfb3bda3c0b8ecf
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-02-18 17:51:34 -0800

    clean up code

--- python_quote.py
+++ python_quote.py
@@ -1,18 +1,15 @@
 import random
 
-"""
-Function returns a random quote from list 
-"""
 def random_python_quote(lis):
+    '''
+    Function returns a random quote from list 
+    '''
     rand_index = random.randint(0, len(lis) - 1)
     return lis[rand_index]
 
-"""
-Entry point of program, runs random_python_question() and returns quote
-"""
 if __name__ == '__main__':
     '''
-    List of example quotes 
+    Entry point of program, runs random_python_question() and returns quote
     '''
     quotes =["It's just a flesh wound.", "He's not the Messiah. He's a very naughty boy!", "THIS IS AN EX-PARROT!!"]
 
